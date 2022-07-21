612897bb6ecf8c5107c178b5049b9745f1632e7b
Shobhitkmr <43270097+shobhitkmr1@users.noreply.github.com>
2022-07-21 16:43:59 +0530

    update

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 28.670724045253454, lng: 77.43079366232121};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Ghaziabad, India' // Title Location
     });
 }
